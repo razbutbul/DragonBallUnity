46124938e3fd94ccc2296a714aaa827df0930729
razbutbul <119590104+razbutbul@users.noreply.github.com>
2023-10-03 13:23:37 +0300

    Animations Commit

--- Assets/Scripts/PlayerScripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovment.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public GameObject topRightLimitGameObject;
+    public GameObject BottomLeftLimitGameObject;
+
+    private Vector3 topRightLimit;
+    private Vector3 BottomLeft;
+    private Vector2 input;
+
+
     void Start()
     {
-        
+        topRightLimit = topRightLimitGameObject.transform.position;
+        BottomLeft = BottomLeftLimitGameObject.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+
+        if ((transform.position.x <= BottomLeft.x && input.x == -1)|| (transform.position.x >= topRightLimit.x && input.x == 1)) 
+        {
+            input.x = 0;
+        }
+        if ((transform.position.y <= BottomLeft.y && input.y == -1) || (transform.position.y >= topRightLimit.y && input.y == 1))
+        {
+            input.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+       {
+           Debug.Log("hello");
+        }
+        transform.position += new Vector3(speed * input.x, speed * input.y, 0);
     }
 }
